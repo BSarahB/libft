51787c868519887c5b80551929f4139ee691cecf
BSarahB <bslajta@gmail.com>
2021-01-03 01:14:15 +0100

    adding header

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenmesb <mbenmesb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/03 01:03:11 by mbenmesb          #+#    #+#             */
+/*   Updated: 2021/01/03 01:03:14 by mbenmesb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-// TO DO COMPATIBILITE char *s ft_strlen
+
 char	*ft_strdup(const char *s)
 {
 	char	*dst;
 	char	*s1;
-	s1 = (char *)s; //je l utilise pas uhuh cf
-	int		len_s; //size_t no?
+	s1 = (char *)s;
+	int		len_s;
 
 	len_s = ft_strlen(s1);
 	if (!(dst = (char*)malloc(sizeof(*dst) * (len_s + 1))))
